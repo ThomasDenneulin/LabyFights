783b33468fb13d8d8614e40ae076315ddc23a3ed
ThomasDenneulin <Thomas.denneulin@devinci.fr>
2017-12-10 17:18:40 +0100

    First lines

--- Cell.cs
+++ Cell.cs
@@ -10,5 +10,100 @@ namespace LabyFights
     soit un seul objet, soit un seul combattant (avec zéro ou plusieurs objets).*/
     class Cell
     {
+        private bool n_Wall;
+        private bool s_Wall;
+        private bool e_wall;
+        private bool w_wall;
+        private int cellSize = 5;
+        private bool visited;
+
+        public Cell()
+        {
+            this.n_Wall = true;
+            this.s_Wall = true;
+            this.e_wall = true;
+            this.w_wall = true;
+            this.visited = false;
+        }
+        #region Get/Set
+
+        public bool N_Wall
+        {
+            get
+            {
+                return n_Wall;
+            }
+
+            set
+            {
+                n_Wall = value;
+            }
+        }
+
+        public bool S_Wall
+        {
+            get
+            {
+                return s_Wall;
+            }
+
+            set
+            {
+                s_Wall = value;
+            }
+        }
+
+        public bool E_wall
+        {
+            get
+            {
+                return e_wall;
+            }
+
+            set
+            {
+                e_wall = value;
+            }
+        }
+
+        public bool W_wall
+        {
+            get
+            {
+                return w_wall;
+            }
+
+            set
+            {
+                w_wall = value;
+            }
+        }
+
+        public int CellSize
+        {
+            get
+            {
+                return cellSize;
+            }
+
+            set
+            {
+                cellSize = value;
+            }
+        }
+
+        public bool Visited
+        {
+            get
+            {
+                return visited;
+            }
+
+            set
+            {
+                visited = value;
+            }
+        }
+        #endregion
     }
 }
--- Maze.cs
+++ Maze.cs
@@ -10,5 +10,47 @@ namespace LabyFights
     rectangulaire entièrement entouré de murs. Les murs et la sortie sont des cases particulières*/
     class Maze
     {
+        private Cell[,] maze;
+        private int width;
+        private int height;
+
+        public Maze(int width,int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.maze = new Cell[height, width];
+            for(int row = 0; row < height; row++)
+            {
+                for(int col = 0; col < width; width++)
+                {
+                    this.maze[row, col] = new Cell();
+                }
+            }
+        }
+
+        //Do it with tuple class
+        private Cell SelectNeighbour(int row,int col)
+        {
+            List<Cell> cellList = new List<Cell>();
+
+            if (row > 0 && !this.maze[row-1,col].Visited) //North neighbour
+            {
+                cellList.Add(this.maze[row - 1, col]);
+            }
+            if(row < this.maze.GetLength(0) - 1) //South neighbour
+            {
+                cellList.Add(this.maze[row + 1, col]);
+            }
+            if(col > 0) //West neighbour
+            {
+                cellList.Add(this.maze[row, col - 1]);
+            }
+            if(col < this.maze.GetLength(1) - 1) //East neigbour
+            {
+                cellList.Add(this.maze[row, col + 1]);
+            }
+
+            return new Cell();
+        }
     }
 }
