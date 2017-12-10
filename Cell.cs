b2e41fd1704cd06d4d65784f5ae7c826ed572e65
ThomasDenneulin <Thomas.denneulin@devinci.fr>
2017-12-10 16:56:19 +0100

    Add class

---
+++ Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyFights
+{
+    /*Une case qui n’est pas un mur peut être libre ou occupée. Une case occupée peut contenir
+    soit un seul objet, soit un seul combattant (avec zéro ou plusieurs objets).*/
+    class Cell
+    {
+    }
+}
---
+++ Maze.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyFights
+{
+    /*Le labyrinthe est une entité unique, jouant le rôle d’élément central. Il est de forme
+    rectangulaire entièrement entouré de murs. Les murs et la sortie sont des cases particulières*/
+    class Maze
+    {
+    }
+}
